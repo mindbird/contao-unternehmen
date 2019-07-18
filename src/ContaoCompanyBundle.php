8c5b9c8a4b7907b2ae65a1a0acac1cf96bcaf8e5
Florian Otto <florian.otto@mindbird.de>
2019-07-18 15:21:07 +0200

    WOD

--- src/ContaoCompanyBundle.php
+++ src/ContaoCompanyBundle.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * This file is part of [mindbird/contao-ce-box].
- *
- * (c) mindbird
- *
- * @license LGPL-3.0-or-later
- */
-
 namespace Mindbird\Contao\Company;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
