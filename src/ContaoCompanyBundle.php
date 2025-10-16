de36f74a264fa833027a6b3a95ef09eb17480b56
Florian Otto <florian.otto@mindbird.de>
2025-10-16 10:34:42 +0200

    Refactor ContaoCompanyBundle to extend AbstractBundle and load services configuration

--- src/ContaoCompanyBundle.php
+++ src/ContaoCompanyBundle.php
@@ -2,8 +2,15 @@
 
 namespace Mindbird\Contao\Company;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ContaoCompanyBundle extends Bundle
+class ContaoCompanyBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
 }
