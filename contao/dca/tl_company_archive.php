436a6378bc04bcf0f66a3679695c37a312e44d6e
Florian Otto <florian.otto@mindbird.de>
2025-10-17 14:12:42 +0200

    Refactor operations in tl_company_archive.php for improved clarity and maintainability

--- contao/dca/tl_company_archive.php
+++ contao/dca/tl_company_archive.php
@@ -31,42 +31,13 @@ $GLOBALS ['TL_DCA'] ['tl_company_archive'] = [
             ],
             'format' => '%s'
         ],
-        'global_operations' => [
-            'all' => [
-                'label' => &$GLOBALS ['TL_LANG'] ['MSC'] ['all'],
-                'href' => 'act=select',
-                'class' => 'header_edit_all',
-                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"'
-            ]
-        ],
         'operations' => [
-            'edit' => [
-                'label' => &$GLOBALS ['TL_LANG'] ['tl_company_archive'] ['edit'],
-                'href' => 'table=tl_company',
-                'icon' => 'edit.gif'
-            ],
-            'editheader' => [
-                'label' => &$GLOBALS ['TL_LANG'] ['tl_company_archive'] ['editheader'],
-                'href' => 'act=edit',
-                'icon' => 'header.gif'
-            ],
-            'copy' => [
-                'label' => &$GLOBALS ['TL_LANG'] ['tl_company_archive'] ['copy'],
-                'href' => 'act=copy',
-                'icon' => 'copy.gif'
-            ],
-            'delete' => [
-                'label' => &$GLOBALS ['TL_LANG'] ['tl_company_archive'] ['delete'],
-                'href' => 'act=delete',
-                'icon' => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS ['TL_LANG'] ['MSC'] ['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
-            ],
-            'show' => [
-                'label' => &$GLOBALS ['TL_LANG'] ['tl_company_archive'] ['show'],
-                'href' => 'act=show',
-                'icon' => 'show.gif'
-            ]
-        ]
+            '!edit',
+            '!children',
+            '!copy',
+            'cut',
+            'delete'
+        ],
     ],
 
     // Palettes
